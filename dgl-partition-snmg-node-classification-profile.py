d992e02058334e43b5da5a820752c63fd619f8b8
wanghongsheng01 <2496533749@qq.com>
2022-09-05 13:02:14 +0800

    fix metis bug; understand onid, gnid, local node id, gpb, id in mfgs

--- dgl-partition-snmg-node-classification-profile.py
+++ dgl-partition-snmg-node-classification-profile.py
@@ -92,16 +92,27 @@ def run(proc_id, devices, args):
     
     # 1. get local partitioned graph with: local_graph, local_nfeat, local_efeat, gpb, graphname 
     parti_results = metis.partition_graph(graph, args.dataset, args.num_gpu, len(sampling_param), proc_id)
-    # gpucacher = cache.GraphCache(graph, proc_id)
-    # for item in parti_results:
-    #     print(type(item)) # dgl.heterograph.DGLHeteroGraph, dict, dict, dgl.distributed.graph_partition_book.RangePartitionBook, str; 其中node_nfeat存储的就是该部分图的每个点的feat(无HALO点)， local_graph是包含HALO点的；book包含每个partion中的包含哪些全局graph relabel后的连续点的id；
-    # gpb.nid2localnid(gnid, partid)得到local node id;
-    # nid2partid(gnid)得到global node id -> partition id;
-    # partid2nids(partid)得到partition id -> global node id；
-    local_graph, local_nfeat, local_efeat, gpb, graphname = parti_results
-    # for k in local_nfeat:
-    #     print(k, local_nfeat[k], local_nfeat[k].size())
-    print(gpb.nid2localnid(1), gpb.nid2partid(100), gpb.partid2nids(1))
+    
+    # gnid2onid=[a, b, c...] a表示原图的node id，remap后对应的id是0，依次类推; 
+    # 我们把remap后从0开始的这些每个partition连续的id称为global id (gnid)，原始图的id称为original id(onid)
+    # gpb.nid2localnid(gnid, partid)得到local node id;其中partid只能是当前proc分配到的partion的id，不能查询远程的partition
+    # gpb.nid2partid(gnid)得到global node id -> partition id;
+    # gpb.partid2nids(partid)得到partition id -> global node id；这里的global node id是连续的；
+    # local_feat是
+    local_graph, local_nfeat, local_efeat, gpb, graphname, gnid2onid, geid2oeid = parti_results
+    
+    print(gnid2onid[0], gnid2onid[1], geid2oeid[0], geid2oeid[1], len(gnid2onid), len(geid2oeid))
+    print('proc_id:', proc_id, gpb.nid2localnid(0, proc_id), gpb.nid2localnid(42693, proc_id), gpb.nid2partid(42690),gpb.nid2partid(42691), gpb.partid2nids(proc_id))
+    
+    if proc_id==0:
+        print(graph, local_graph) # Graph(num_nodes=169343, num_edges=2332486, ndata_schemes={'year': Scheme(shape=(1,), dtype=torch.int64), 'feat': Scheme(shape=(128,), dtype=torch.float32), 'label': Scheme(shape=(), dtype=torch.int64)} edata_schemes={}) 
+        #Graph(num_nodes=153373, num_edges=2102947, ndata_schemes={'orig_id': Scheme(shape=(), dtype=torch.int64), 'part_id': Scheme(shape=(), dtype=torch.int64), 'inner_node': Scheme(shape=(), dtype=torch.int32), '_ID': Scheme(shape=(), dtype=torch.int64)} edata_schemes={'inner_edge': Scheme(shape=(), dtype=torch.int8), 'orig_id': Scheme(shape=(), dtype=torch.int64), '_ID': Scheme(shape=(), dtype=torch.int64)})
+
+        # 下面展示从子图访问和原图访问相同的点以及node id
+        print(graph.ndata['feat'][121586]) # 从原图访问某个点，改点对应part0的0号点
+        print(gpb.partid2nids(0)[0], gnid2onid[gpb.partid2nids(0)[0]], local_nfeat['_N/feat'][0],) # 从子图的属性中访问同一个点，打印gnid, onid,和feat
+
+
     
     # 2. local gpu-cache
     n_local_nfeats, nfeat_dim = list(local_nfeat.values())[0].size()
@@ -121,6 +132,8 @@ def run(proc_id, devices, args):
             with tqdm.tqdm(train_dataloader) as tq:
                 st = time.time()
                 for step, (input_nodes, output_nodes, mfgs) in enumerate(tq):
+                    # if 121586 in input_nodes:
+                    #     print('121586 in mfgs with feat:', mfgs[0].srcdata['feat'][input_nodes.tolist().index(121586)]) # 说明mfgs中的点id是onid
                     avg_time_sampling.append(time.time() - st)
                     with torch.autograd.profiler.record_function('cpu-gpu-feat-label-load'):
                         with timer.Timer(device=f"cuda:{proc_id}") as t:
@@ -171,7 +184,7 @@ def run(proc_id, devices, args):
 
 def parse_args_func(argv):
     parser = argparse.ArgumentParser(description='GNN Training')
-    parser.add_argument('-d', '--dataset', default="ogbn-products", type=str, choices=['ogbn-arxiv', 'ogbn-products', 'ogbn-proteins', 'ogbn-mag'], help='training dataset name')
+    parser.add_argument('-d', '--dataset', default="ogbn-arxiv", type=str, choices=['ogbn-arxiv', 'ogbn-products', 'ogbn-proteins', 'ogbn-mag'], help='training dataset name')
     parser.add_argument('-ngpu', '--num-gpu', default=4, type=int, help='# of gpus to train gnn with DDP')
     parser.add_argument('-s', '--sampling', default="10-10-10", type=str, help='neighborhood sampling method parameters')
     parser.add_argument('-hd', '--hidden-size', default=256, type=int, help='hidden dimension size')
