befa69a59859cb231eafef5fead2d05ca277f7f3
nustart0720 <1317732065@qq.com>
2023-03-12 11:51:22 +0800

    cha2

---
+++ utils/README.md
@@ -0,0 +1,3 @@
+# Requirements
++ pip3 install GitPython -i https://pypi.tuna.tsinghua.edu.cn/simple
++ sudo apt-get install sshpass
---
+++ utils/sync_git_modifies.py
@@ -0,0 +1,27 @@
+import git
+import os
+
+# 执行本文件需要到git仓库根目录，否则get_modifed_files函数执行报错
+
+def get_modifed_files(gitpath):
+    repo = git.Repo(gitpath)
+    modified_files = [item.a_path for item in repo.index.diff(None) if item.change_type != "D"]
+    # print(modified_files)
+    return modified_files
+
+def scp_modifed_files_to_another_mathine_under_same_path(remote_ip, pswd):
+    files_lst = get_modifed_files(os.getcwd())
+    print(files_lst)
+    instruct = input(f'将传输上述文件到{remote_ip}同名路径下，请确认文件路径是否正确[y/n]：')
+    if instruct=='y':
+        for filename in files_lst:
+            os.system(f'sshpass -p {pswd} scp {filename} {remote_ip}:{os.path.join(os.getcwd(), filename)}')
+            print(f'scp {filename} {remote_ip}:{os.path.join(os.getcwd(), filename)}执行结束')
+    elif instruct == 'n':
+        print('未开始传输，强制结束')
+    else:
+        print('非法输入，请选择y/n')
+
+remote_IP = input('请输入目的机器域名或ip：')
+pswd = input('请输入ssh remote_IP 的密码：')
+scp_modifed_files_to_another_mathine_under_same_path(remote_ip=remote_IP, pswd=pswd)
