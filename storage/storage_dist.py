e743f1fdcc8538d1cfb897ed8581319438c82f5e
nustart0720 <1317732065@qq.com>
2023-03-15 18:19:09 +0800

    WIP: merge and fetch multiple nfs at once

--- storage/storage_dist.py
+++ storage/storage_dist.py
@@ -92,6 +92,76 @@ class DistCacheClient:
                 logging.debug(f'Final nodeflow._node_frames:{i}, frame["features"].size(): {frame["features"].size()}\n')
                 nodeflow._node_frames[i] = FrameRef(Frame(frame))
     
+
+    def fetch_multiple_nfs(self, nfs):
+        with torch.autograd.profiler.record_function('get nf_nids'):
+            nf_nids = [] # ngpu个子树的nid
+            offsets = [] # ngpu个子树不同层的划分界点
+            for j in range(self.world_size):
+                nf_nids.append(nfs[j]._node_mapping.tousertensor().cuda(self.gpuid))
+                offsets.append(nfs[j]._layer_offsets)
+        
+        for i in range(nfs[0].num_layers):
+            tnid = [] # ngpu个树的第i层的nid
+            tnids_flat = []
+            for j in range(self.world_size):
+                tnid.append(nf_nids[j][offsets[j][i]:offsets[j][i+1]])
+                tnid[j] = tnid[j].tolist()
+                tnids_flat.extend(tnid)
+            
+            # create frames
+            n_rows = []
+            with torch.autograd.profiler.record_function('fetch feat overhead'):
+                with torch.cuda.device(self.gpuid):
+                    frames = [] # ngpu个树第i层的nid构成的frame
+                    for j in range(self.world_size):
+                        frames.append({name: torch.empty(len(tnid[j]), self.dims[name]) for name in self.dims})
+                        n_rows.extend([len(tnid[j]) for name in self.dims])
+            # fetch features from cache server
+            frame_id, row_st_idx, row_ed_idx = 0, 0, 0
+            with torch.autograd.profiler.record_function('fetch feat from cache server'):
+                for sub_features in self.get_stream_feats_from_server(tnids_flat):
+                    for name in self.dims:
+                        sub_tensor = torch.frombuffer(sub_features, dtype=torch.float32).reshape(-1, self.feat_dim)
+                        n_sub_tensor_row = sub_tensor.shape[0]
+                        # 如果填充得下当前的frame
+                        if (row_st_idx + n_sub_tensor_row) <= n_rows[frame_id]:
+                            row_ed_idx += n_sub_tensor_row
+                            frames[frame_id][name][row_st_idx:row_ed_idx, :] = sub_tensor
+                            # 如果正好填充满了当前的frame
+                            if row_ed_idx == n_rows[frame_id]:
+                                row_st_idx, row_ed_idx = 0, 0
+                                frame_id += 1
+                            else:
+                                row_st_idx = row_ed_idx
+                        # 如果当前frame填充不下，可能往下填充一个或多个
+                        else:
+                            sub_tensor_st_idx = 0
+                            while(n_sub_tensor_row): # 直到将要填充的sub_tensor为空
+                                cur_frame_remains_rows = n_rows[frame_id] - row_ed_idx
+                                fill_rows = min(cur_frame_remains_rows, n_sub_tensor_row)
+                                row_ed_idx += fill_rows
+                                frames[frame_id][name][row_st_idx:row_ed_idx, :] = sub_tensor[sub_tensor_st_idx:sub_tensor_st_idx + fill_rows]
+                                sub_tensor_st_idx += fill_rows
+                                if (n_sub_tensor_row - fill_rows) > 0:
+                                    # 为填充下一个frame准备
+                                    n_sub_tensor_row -= fill_rows
+                                    frame_id += 1
+                                    row_st_idx = row_ed_idx = 0
+                                else:
+                                    row_st_idx = row_ed_idx
+            with torch.autograd.profiler.record_function('move feats from CPU to GPU'):
+                # move multiple features from cpu memory to gpu memory
+                for j in range(self.world_size):
+                    for name in self.dims:
+                        frames[j][name].data = frames[j][name].data.cuda(self.gpuid)
+            # attach features to nodeflow
+            with torch.autograd.profiler.record_function('asign frame to nodeflow'):
+                for j in range(self.world_size):
+                    logging.debug(f'Final nfs._node_frames:{i}, frames[j]["features"].size(): {frames[j]["features"].size()}\n')
+                    nfs[j]._node_frames[i] = FrameRef(Frame(frames[j]))
+            
+
     def get_feat_dim(self):
         response = self.stub.DCSubmit(distcache_pb2.DCRequest(
         type=distcache_pb2.get_feature_dim), timeout=1000) # response is DCReply type response
