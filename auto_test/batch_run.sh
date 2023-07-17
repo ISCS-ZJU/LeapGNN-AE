b6f5d29ee60963974cfed6fff921d4e55398e81a
solitude-boy <weijianchen@zju.edu.cn>
2023-07-17 10:44:46 +0800

    fix filename error

--- auto_test/batch_run.sh
+++ auto_test/batch_run.sh
@@ -31,7 +31,7 @@
 # bs=8000 p3
 # python3 clients_start.py --model_name gcn --sampling 2-2 --run_client_idx 4 --dataset ogbn_products0 --n_epochs 3 --batch_size 8000
 # python3 clients_start.py --model_name gcn --sampling 10-10 --run_client_idx 4 --dataset ogbn_products0 --n_epochs 3 --batch_size 8000
-python3 clients_start.py --model_name graphsage --sampling 10-10 --run_client_idx 4 --dataset ogbn_products0 --n_epochs 3 --batch_size 8000
-python3 clients_start.py --model_name gat --sampling 10-10 --run_client_idx 4 --dataset ogbn_products0 --n_epochs 3 --batch_size 8000
-python3 clients_start.py --model_name graphsage --sampling 10-10 --run_client_idx 4 --dataset ogbn_arxiv0 --n_epochs 3 --batch_size 8000
-python3 clients_start.py --model_name gat --sampling 10-10 --run_client_idx 4 --dataset ogbn_arxiv0 --n_epochs 3 --batch_size 8000
+# python3 clients_start.py --model_name graphsage --sampling 10-10 --run_client_idx 4 --dataset ogbn_products0 --n_epochs 3 --batch_size 8000
+# python3 clients_start.py --model_name gat --sampling 10-10 --run_client_idx 4 --dataset ogbn_products0 --n_epochs 3 --batch_size 8000
+# python3 clients_start.py --model_name graphsage --sampling 10-10 --run_client_idx 4 --dataset ogbn_arxiv0 --n_epochs 3 --batch_size 8000
+# python3 clients_start.py --model_name gat --sampling 10-10 --run_client_idx 4 --dataset ogbn_arxiv0 --n_epochs 3 --batch_size 8000
