e85f2a9d38b742578f8f7cd32091811d0f226887
swordfate <47095443+swordfate@users.noreply.github.com>
2024-12-26 18:43:07 +0800

    Create acc.sh

---
+++ test/acc.sh
@@ -0,0 +1,4 @@
+# This script analyzes the accuracy of previous runs based on log files. 
+# It reads all .log files, extracts the maximum accuracy (Max acc:) from each file
+# Due to space constraints, the results for Reddit were not included in the paper.
+python3 ../acc.py
